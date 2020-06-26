58141bade6b2f140687df2a5654393a803bb832b
Lindsay Pasricha <lindsayw@chromium.org>
2020-06-26 16:22:02 +0000

    Revert "Reland "[iOS] Remove unused ChromeIdentityInteractionManager APIs.""
    This reverts commit 3293e9467c1a864a3738e88a4326b47089ad7700.
    
    Reason for revert: AddAccountSigninManagerTests are failing again
    
    Original change's description:
    > Reland "[iOS] Remove unused ChromeIdentityInteractionManager APIs."
    > 
    > This reverts commit 11f8ce83d103c7109fd8cd999e42b69283878ed7.
    > 
    > Reason for revert: Landed the fix to SigninCoordinator EarlGrey tests
    > in patch 2264057.
    > 
    > Original change's description:
    > > Revert "[iOS] Remove unused ChromeIdentityInteractionManager APIs."
    > >
    > > This reverts commit 053f61242771b0467be5dd59ced9582d45393961.
    > >
    > > Reason for revert:
    > > AddAccountSigninManagerTest fail on iOS 12:
    > > https://ci.chromium.org/p/chrome/builders/ci/iphone-device/9356
    > 
    > Bug: 1078815
    > Change-Id: Iba786bb1caa7dbbb1b8aa3c4d2feeb5e24674922
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2270085
    > Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    > Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    > Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#783001}
    
    TBR=jlebel@chromium.org,fernandex@chromium.org
    
    Change-Id: Ic3884c804f6ce91240a3d15e4c8134ae255fc498
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1078815
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2270617
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Lindsay Pasricha <lindsayw@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#783019}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 763341597b3a1593e5b9c670bfb637ac847f529f

--- public/provider/chrome/browser/signin/fake_chrome_identity_interaction_manager.mm
+++ public/provider/chrome/browser/signin/fake_chrome_identity_interaction_manager.mm
@@ -24,6 +24,7 @@
 @interface FakeChromeIdentityInteractionManager () {
   SigninCompletionCallback _completionCallback;
   UIViewController* _viewController;
+  BOOL _isCanceling;
 }
 
 @end
@@ -96,6 +97,33 @@
 @implementation FakeChromeIdentityInteractionManager
 
 @synthesize fakeIdentity = _fakeIdentity;
+
+- (BOOL)isCanceling {
+  return _isCanceling;
+}
+
+- (void)addAccountWithCompletion:(SigninCompletionCallback)completion {
+  _completionCallback = completion;
+  _viewController =
+      [[FakeAddAccountViewController alloc] initWithInteractionManager:self];
+  [self.delegate interactionManager:self
+              presentViewController:_viewController
+                           animated:YES
+                         completion:nil];
+}
+
+- (void)reauthenticateUserWithID:(NSString*)userID
+                           email:(NSString*)userEmail
+                      completion:(SigninCompletionCallback)completion {
+  [self addAccountWithCompletion:completion];
+}
+
+- (void)cancelAndDismissAnimated:(BOOL)animated {
+  _isCanceling = YES;
+  [self dismissAndRunCompletionCallbackWithError:[self canceledError]
+                                        animated:animated];
+  _isCanceling = NO;
+}
 
 - (void)addAccountWithPresentingViewController:(UIViewController*)viewController
                                     completion:

420c8e8e9cca3cbd2a593c732dc3ee19fadfa68e
edchin <edchin@chromium.org>
2020-07-04 04:37:11 +0000

    [ios] Cleanup import in ios/c/b/ui/reading_list
    crrev.com/c/2277245 accidentally added an unnecessary import
    and dependency in reading_list. This CL fixes that.
    
    Change-Id: I8493f0f0beb440ef5207719e5be16191acbbeabc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2276309
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: edchin <edchin@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#785155}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: ff9cef6e03e36f18b543b10b242ccade663453ae

--- chrome/browser/ui/reading_list/reading_list_table_view_controller.mm
+++ chrome/browser/ui/reading_list/reading_list_table_view_controller.mm
@@ -31,7 +31,6 @@
 #import "ios/chrome/browser/ui/table_view/cells/table_view_text_header_footer_item.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
 #include "ios/chrome/grit/ios_strings.h"
-#import "net/base/mac/url_conversions.h"
 #include "ui/base/l10n/l10n_util_mac.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)

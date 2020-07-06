5fb549ac19e0992aadf1dbd9a1e3a564eb4f8492
Jérôme Lebel <jlebel@chromium.org>
2020-07-06 18:24:05 +0000

    [iOS] Using completion when canceling IdentityInteractionManager
    When IdentityInteractionManager is canceled, the interruption block
    can be directly given to the completion of:
    -[IdentityInteractionManager cancelAddAccountWithAnimation:completion:]
    
    Related to:
    crrev.com/c/2207178
    
    Fixed: 1072347
    Change-Id: I3a053d2fca481a5317895fad2ca6823fff7c32ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2278047
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#785468}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: c9cf1ba810bcb76b2997e7e0e66dfa8455997d49

--- public/provider/chrome/browser/signin/fake_chrome_identity_interaction_manager.mm
+++ public/provider/chrome/browser/signin/fake_chrome_identity_interaction_manager.mm
@@ -120,29 +120,35 @@
 - (void)cancelAddAccountWithAnimation:(BOOL)animated
                            completion:(void (^)(void))completion {
   [self dismissAndRunCompletionCallbackWithError:[self canceledError]
-                                        animated:animated];
+                                        animated:animated
+                                      completion:completion];
 }
 
 - (void)addAccountViewControllerDidTapSignIn {
   ios::FakeChromeIdentityService::GetInstanceFromChromeProvider()
       ->AddIdentity(_fakeIdentity);
-  [self dismissAndRunCompletionCallbackWithError:nil animated:YES];
+  [self dismissAndRunCompletionCallbackWithError:nil
+                                        animated:YES
+                                      completion:nil];
 }
 
 - (void)addAccountViewControllerDidTapCancel {
   [self dismissAndRunCompletionCallbackWithError:[self canceledError]
-                                        animated:YES];
+                                        animated:YES
+                                      completion:nil];
 }
 
 - (void)addAccountViewControllerDidThrowUnhandledError {
   [self dismissAndRunCompletionCallbackWithError:[self unhandledError]
-                                        animated:YES];
+                                        animated:YES
+                                      completion:nil];
 }
 
 #pragma mark Helper
 
 - (void)dismissAndRunCompletionCallbackWithError:(NSError*)error
-                                        animated:(BOOL)animated {
+                                        animated:(BOOL)animated
+                                      completion:(void (^)(void))completion {
   if (!_viewController) {
     [self runCompletionCallbackWithError:error];
     return;
@@ -151,6 +157,9 @@
       dismissViewControllerAnimated:animated
                          completion:^{
                            [self runCompletionCallbackWithError:error];
+                           if (completion) {
+                             completion();
+                           }
                          }];
 }
 

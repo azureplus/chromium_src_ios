3a2b5df2bf719c310bfe1ee08c034fa86fed5c2f
Viktor Semeniuk <vsemeniuk@google.com>
2020-07-17 15:57:04 +0000

    [iOS][Password Check] Password Details View Controller
    This change is a foundation for refurbished version of Passwords
    Details screen. For now this screen shows only website, username and
    masked password. Also it allows to edit password although at the moment
    it has no effect. This screen is shown only for compromised credentials.
    
    Bug: 1075494
    Change-Id: I36de403dba0c560aa305e13b029351366629c9e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2295141
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#789499}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 727fdb5bd29e7d43d2721e7144610e28d470e999

--- chrome/browser/ui/settings/password/password_details/password_details_view_controller.mm
+++ chrome/browser/ui/settings/password/password_details/password_details_view_controller.mm
@@ -7,11 +7,32 @@
 #include "base/mac/foundation_util.h"
 #import "ios/chrome/browser/ui/settings/password/password_details/password_details.h"
 #import "ios/chrome/browser/ui/settings/password/password_details/password_details_consumer.h"
+#import "ios/chrome/browser/ui/settings/password/password_details/password_details_handler.h"
 #import "ios/chrome/browser/ui/settings/password/password_details/password_details_table_view_constants.h"
+#import "ios/chrome/browser/ui/settings/password/password_details/password_details_view_controller_delegate.h"
+#import "ios/chrome/browser/ui/table_view/cells/table_view_cells_constants.h"
+#import "ios/chrome/browser/ui/table_view/cells/table_view_text_edit_item.h"
+#include "ios/chrome/grit/ios_strings.h"
+#include "ui/base/l10n/l10n_util_mac.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
+
+namespace {
+
+typedef NS_ENUM(NSInteger, SectionIdentifier) {
+  SectionIdentifierPassword = kSectionIdentifierEnumZero,
+  // TODO:(crbug.com/1075494) - Add password compromised section.
+};
+
+typedef NS_ENUM(NSInteger, ItemType) {
+  ItemTypeWebsite = kItemTypeEnumZero,
+  ItemTypeUsername,
+  ItemTypePassword,
+};
+
+}  // namespace
 
 @interface PasswordDetailsViewController ()
 
@@ -32,10 +53,133 @@
   [self loadModel];
 }
 
+- (void)viewDidDisappear:(BOOL)animated {
+  [self.handler passwordDetailsViewControllerDidDisappear];
+  [super viewDidDisappear:animated];
+}
+
 #pragma mark - ChromeTableViewController
+
+- (void)editButtonPressed {
+  [super editButtonPressed];
+
+  if (!self.tableView.editing) {
+    // TODO:(crbug.com/1075494) - Update |_password| accordingly.
+    [self.delegate passwordDetailsViewController:self
+                          didEditPasswordDetails:self.password];
+  }
+
+  [self loadModel];
+  [self reconfigureCellsForItems:
+            [self.tableViewModel
+                itemsInSectionWithIdentifier:SectionIdentifierPassword]];
+}
 
 - (void)loadModel {
   [super loadModel];
+  self.title = self.password.origin;
+
+  TableViewModel* model = self.tableViewModel;
+  [model addSectionWithIdentifier:SectionIdentifierPassword];
+
+  [model addItem:[self websiteItem]
+      toSectionWithIdentifier:SectionIdentifierPassword];
+
+  [model addItem:[self usernameItem]
+      toSectionWithIdentifier:SectionIdentifierPassword];
+
+  [model addItem:[self passwordItem]
+      toSectionWithIdentifier:SectionIdentifierPassword];
+}
+
+#pragma mark - Items
+
+- (TableViewTextEditItem*)websiteItem {
+  TableViewTextEditItem* item =
+      [[TableViewTextEditItem alloc] initWithType:ItemTypeWebsite];
+  item.textFieldName = l10n_util::GetNSString(IDS_IOS_SHOW_PASSWORD_VIEW_SITE);
+  item.textFieldValue = self.password.website;
+  item.textFieldEnabled = NO;
+  item.hideIcon = YES;
+  return item;
+}
+
+- (TableViewTextEditItem*)usernameItem {
+  TableViewTextEditItem* item =
+      [[TableViewTextEditItem alloc] initWithType:ItemTypeUsername];
+  item.textFieldName =
+      l10n_util::GetNSString(IDS_IOS_SHOW_PASSWORD_VIEW_USERNAME);
+  item.textFieldValue = self.password.username;
+  item.textFieldEnabled = NO;
+  item.hideIcon = YES;
+  return item;
+}
+
+- (TableViewTextEditItem*)passwordItem {
+  TableViewTextEditItem* item =
+      [[TableViewTextEditItem alloc] initWithType:ItemTypePassword];
+  item.textFieldName =
+      l10n_util::GetNSString(IDS_IOS_SHOW_PASSWORD_VIEW_PASSWORD);
+  item.textFieldValue = kMaskedPassword;
+  item.textFieldEnabled = self.tableView.editing;
+  item.hideIcon = !self.tableView.editing;
+  item.autoCapitalizationType = UITextAutocapitalizationTypeNone;
+  item.keyboardType = UIKeyboardTypeURL;
+  item.returnKeyType = UIReturnKeyDone;
+  // TODO:(crbug.com/1075494) - Add eye icon to view password.
+  return item;
+}
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView*)tableView
+    didSelectRowAtIndexPath:(NSIndexPath*)indexPath {
+  if (self.tableView.editing) {
+    UITableViewCell* cell = [self.tableView cellForRowAtIndexPath:indexPath];
+    TableViewTextEditCell* textFieldCell =
+        base::mac::ObjCCastStrict<TableViewTextEditCell>(cell);
+    [textFieldCell.textField becomeFirstResponder];
+  }
+}
+
+- (UITableViewCellEditingStyle)tableView:(UITableView*)tableView
+           editingStyleForRowAtIndexPath:(NSIndexPath*)indexPath {
+  return UITableViewCellEditingStyleNone;
+}
+
+- (BOOL)tableView:(UITableView*)tableview
+    shouldIndentWhileEditingRowAtIndexPath:(NSIndexPath*)indexPath {
+  return NO;
+}
+
+#pragma mark - UITableViewDataSource
+
+- (UITableViewCell*)tableView:(UITableView*)tableView
+        cellForRowAtIndexPath:(NSIndexPath*)indexPath {
+  UITableViewCell* cell = [super tableView:tableView
+                     cellForRowAtIndexPath:indexPath];
+
+  cell.selectionStyle = UITableViewCellSelectionStyleNone;
+
+  // TODO:(crbug.com/1075494) - Add action to Show/Hide password when user tap
+  // eye icon.
+  TableViewTextEditCell* textFieldCell =
+      base::mac::ObjCCastStrict<TableViewTextEditCell>(cell);
+  textFieldCell.textField.delegate = self;
+  return textFieldCell;
+}
+
+- (BOOL)tableView:(UITableView*)tableView
+    canEditRowAtIndexPath:(NSIndexPath*)indexPath {
+  NSInteger itemType = [self.tableViewModel itemTypeForIndexPath:indexPath];
+  switch (itemType) {
+    case ItemTypeWebsite:
+    case ItemTypeUsername:
+      return NO;
+    case ItemTypePassword:
+      return YES;
+  }
+  return NO;
 }
 
 #pragma mark - PasswordDetailsConsumer
@@ -43,6 +187,18 @@
 - (void)setPassword:(PasswordDetails*)password {
   _password = password;
   [self reloadData];
+}
+
+#pragma mark - Private
+
+// Called when user tapped Delete button during editing. It means presented
+// password should be deleted.
+- (void)deleteItems:(NSArray<NSIndexPath*>*)indexPaths {
+  // TODO:(crbug.com/1075494) - Show Confirmation dialog and delete password.
+}
+
+- (BOOL)shouldHideToolbar {
+  return !self.editing;
 }
 
 @end
---
+++ chrome/browser/ui/settings/password/password_details/password_detials_view_controller_unittest.mm
@@ -0,0 +1,127 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/settings/password/password_details/password_details_view_controller.h"
+
+#include <memory>
+
+#include "base/strings/utf_string_conversions.h"
+#include "components/autofill/core/common/password_form.h"
+#include "ios/chrome/browser/browser_state/test_chrome_browser_state.h"
+#import "ios/chrome/browser/ui/settings/password/password_details/password_details.h"
+#import "ios/chrome/browser/ui/settings/password/password_details/password_details_consumer.h"
+#import "ios/chrome/browser/ui/settings/password/password_details/password_details_handler.h"
+#import "ios/chrome/browser/ui/settings/password/password_details/password_details_view_controller_delegate.h"
+#import "ios/chrome/browser/ui/table_view/cells/table_view_cells_constants.h"
+#import "ios/chrome/browser/ui/table_view/cells/table_view_text_edit_item.h"
+#import "ios/chrome/browser/ui/table_view/chrome_table_view_controller_test.h"
+#include "ios/chrome/grit/ios_chromium_strings.h"
+#include "ios/chrome/grit/ios_strings.h"
+#include "ios/web/public/test/web_task_environment.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "testing/gtest_mac.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+// Test class that conforms to PasswordDetailsHanler in order to test the
+// presenter methods are called correctly.
+@interface FakePasswordDetailsHandler : NSObject <PasswordDetailsHandler>
+@end
+
+@implementation FakePasswordDetailsHandler
+
+// Called when the view controller was dismissed.
+- (void)passwordDetailsViewControllerDidDisappear {
+}
+
+@end
+
+// Test class that conforms to PasswordDetailsViewControllerDelegate in order to
+// test the delegate methods are called correctly.
+@interface FakePasswordDetailsDelegate
+    : NSObject <PasswordDetailsViewControllerDelegate>
+@end
+
+@implementation FakePasswordDetailsDelegate
+
+- (void)passwordDetailsViewController:
+            (PasswordDetailsViewController*)viewController
+               didEditPasswordDetails:(PasswordDetails*)password {
+}
+
+@end
+
+// Unit tests for PasswordIssuesTableViewController.
+class PasswordDetailsViewControllerTest : public ChromeTableViewControllerTest {
+ protected:
+  PasswordDetailsViewControllerTest() {
+    handler_ = [[FakePasswordDetailsHandler alloc] init];
+    delegate_ = [[FakePasswordDetailsDelegate alloc] init];
+  }
+
+  ChromeTableViewController* InstantiateController() override {
+    PasswordDetailsViewController* controller =
+        [[PasswordDetailsViewController alloc]
+            initWithStyle:UITableViewStylePlain];
+    controller.handler = handler_;
+    controller.delegate = delegate_;
+    return controller;
+  }
+
+  void ShowPassword() {
+    auto form = autofill::PasswordForm();
+    form.url = GURL("http://www.example.com/");
+    form.action = GURL("http://www.example.com/accounts/Login");
+    form.username_element = base::ASCIIToUTF16("Email");
+    form.username_value = base::ASCIIToUTF16("test@egmail.com");
+    form.password_element = base::ASCIIToUTF16("Passwd");
+    form.password_value = base::ASCIIToUTF16("test");
+    form.submit_element = base::ASCIIToUTF16("signIn");
+    form.signon_realm = "http://www.example.com/";
+    form.scheme = autofill::PasswordForm::Scheme::kHtml;
+    PasswordDetails* password =
+        [[PasswordDetails alloc] initWithPasswordForm:form];
+
+    PasswordDetailsViewController* passwords_controller =
+        static_cast<PasswordDetailsViewController*>(controller());
+    [passwords_controller setPassword:password];
+  }
+
+  void CheckEditCellText(NSString* expected_text, int section, int item) {
+    TableViewTextEditItem* cell =
+        static_cast<TableViewTextEditItem*>(GetTableViewItem(section, item));
+    EXPECT_NSEQ(expected_text, cell.textFieldValue);
+  }
+
+  FakePasswordDetailsHandler* handler() { return handler_; }
+  FakePasswordDetailsDelegate* delegate() { return delegate_; }
+
+ private:
+  FakePasswordDetailsHandler* handler_;
+  FakePasswordDetailsDelegate* delegate_;
+};
+
+// Tests PasswordDetailsViewController is set up with appropriate items
+// and sections.
+TEST_F(PasswordDetailsViewControllerTest, TestModel) {
+  CreateController();
+  CheckController();
+  EXPECT_EQ(1, NumberOfSections());
+
+  EXPECT_EQ(3, NumberOfItemsInSection(0));
+}
+
+// Tests that password is displayed properly.
+TEST_F(PasswordDetailsViewControllerTest, TestPassword) {
+  ShowPassword();
+  EXPECT_EQ(1, NumberOfSections());
+  EXPECT_EQ(3, NumberOfItemsInSection(0));
+
+  EXPECT_NSEQ(@"example.com", controller().title);
+  CheckEditCellText(@"http://www.example.com/", 0, 0);
+  CheckEditCellText(@"test@egmail.com", 0, 1);
+  CheckEditCellText(kMaskedPassword, 0, 2);
+}

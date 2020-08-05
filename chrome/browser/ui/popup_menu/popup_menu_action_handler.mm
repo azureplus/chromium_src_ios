35d96e0fc26ed0b4ad37aea478ef0029f87487cd
Tina Wang <tinazwang@google.com>
2020-08-05 23:36:54 +0000

    [ios] Add "Learn More" link to enterprise menu item
    Add "Learn More" to enterprise info item in 3-dot menu. When users click
    on it, the page will be navigated to chrome://management.
    
    Bug: 1109000
    Change-Id: Idf032d4402f918089a03a17d78848f644948597a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2337100
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#795253}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 7bba861ce133df845c72ced5bf90c9bfa3b9b6f3

--- chrome/browser/ui/popup_menu/popup_menu_action_handler.mm
+++ chrome/browser/ui/popup_menu/popup_menu_action_handler.mm
@@ -24,6 +24,7 @@
 #import "ios/chrome/browser/ui/popup_menu/public/popup_menu_table_view_controller.h"
 #import "ios/chrome/browser/ui/ui_feature_flags.h"
 #import "ios/chrome/browser/window_activities/window_activity_helpers.h"
+#include "url/gurl.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -31,6 +32,12 @@
 
 using base::RecordAction;
 using base::UserMetricsAction;
+
+namespace {
+
+const char kManagementPageURL[] = "chrome://management";
+
+}  // namespace
 
 @implementation PopupMenuActionHandler
 
@@ -203,6 +210,9 @@ using base::UserMetricsAction;
       break;
     }
     case PopupMenuActionEnterpriseInfoMessage:
+      [self.dispatcher
+          openURLInNewTab:[OpenNewTabCommand commandWithURLFromChrome:
+                                                 GURL(kManagementPageURL)]];
       break;
     default:
       NOTREACHED() << "Unexpected identifier";

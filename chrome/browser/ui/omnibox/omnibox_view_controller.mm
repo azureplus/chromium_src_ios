fcc8a110ec0e53ce77e164f734197be426f31538
Nazerke <Nazerke@google.com>
2020-08-11 19:06:53 +0000

    [ios] UI Polish Omnibox: add animation to DSE favicon.
    Current behaviour:
    When focusing the Omnibox, the icon on the far left (G icon, Favicon,
    e.g.) has no animation.
    
    Intended behaviour:
    The icon on the far left should have a short animation starting from an
    opacity of 0 and a scale of 0 towards an opacity of 100 and a scale of 1.
    
    Bug: none.
    Change-Id: I06a90707db4e496fe1aef032e2ce110f392e3d81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2339531
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Nazerke Kalidolda <nazerke@google.com>
    Cr-Original-Commit-Position: refs/heads/master@{#796931}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 281486c28e8370d85d6ffd5df20c3c36b4835438

--- chrome/browser/ui/omnibox/omnibox_view_controller.mm
+++ chrome/browser/ui/omnibox/omnibox_view_controller.mm
@@ -364,7 +364,19 @@ const CGFloat kClearButtonSize = 28.0f;
 #pragma mark - EditViewAnimatee
 
 - (void)setLeadingIconFaded:(BOOL)faded {
-  [self.view setLeadingImageAlpha:faded ? 0 : 1];
+  CATransition* transition = [CATransition animation];
+  transition.duration = 0.3;
+  transition.timingFunction = [CAMediaTimingFunction
+      functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+  transition.type = kCATransitionFade;
+  [self.view.layer addAnimation:transition forKey:nil];
+  if (faded) {
+    [self.view setLeadingImageAlpha:0];
+    [self.view setLeadingImageScale:0];
+  } else {
+    [self.view setLeadingImageAlpha:1];
+    [self.view setLeadingImageScale:1];
+  }
 }
 
 - (void)setClearButtonFaded:(BOOL)faded {

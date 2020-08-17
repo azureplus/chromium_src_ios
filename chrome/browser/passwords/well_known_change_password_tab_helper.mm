cc2c6d2e49d3c506bc637bdffaead5bde31434ac
Jonathan Mengedoht <mengedoht@google.com>
2020-08-17 16:10:08 +0000

    Add processing logic to WellKnownChangePasswordTabHelper
    Bug: 927473
    Change-Id: Icf7010f41392f761117915dac92f4f390b41f2a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2335289
    Commit-Queue: Jonathan Mengedoht <mengedoht@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798659}
    GitOrigin-RevId: be65655c21af2a2c370971a399c0423e42f4814f

--- chrome/browser/passwords/well_known_change_password_tab_helper.mm
+++ chrome/browser/passwords/well_known_change_password_tab_helper.mm
@@ -12,14 +12,13 @@
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #import "ios/web/public/navigation/navigation_context.h"
 #import "net/base/mac/url_conversions.h"
+#include "services/network/public/cpp/shared_url_loader_factory.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
 
-namespace {
 using password_manager::WellKnownChangePasswordTabHelper;
-}
 
 WellKnownChangePasswordTabHelper::WellKnownChangePasswordTabHelper(
     web::WebState* web_state)
@@ -29,26 +28,46 @@ WellKnownChangePasswordTabHelper::WellKnownChangePasswordTabHelper(
 
 WellKnownChangePasswordTabHelper::~WellKnownChangePasswordTabHelper() = default;
 
-void WellKnownChangePasswordTabHelper::DidRedirectNavigation(
+void WellKnownChangePasswordTabHelper::DidStartNavigation(
     web::WebState* web_state,
     web::NavigationContext* navigation_context) {
-  // TODO(crbug.com/927473): handle redirects
+  // |request_url_| is set when the first request goes to
+  // .well-known-change-password. If the navigation url and |request_url_| are
+  // equal, DidStartNavigation() is called for the .well-known/change-password
+  // navigation. Otherwise a different navigation was started.
+  if (!(request_url_.is_valid() &&
+        request_url_ == navigation_context->GetUrl())) {
+    processing_state_ = kInactive;
+  }
 }
 
 web::WebStatePolicyDecider::PolicyDecision
 WellKnownChangePasswordTabHelper::ShouldAllowRequest(
     NSURLRequest* request,
     const RequestInfo& request_info) {
-  const GURL& request_url = net::GURLWithNSURL(request.URL);
-  // Boolean order important. First url then feature flag. Otherwise it messes
-  // with usage statistics of control and experimental group (UMA).
-  if (request_info.target_frame_is_main &&
-      IsWellKnownChangePasswordUrl(request_url) &&
-      base::FeatureList::IsEnabled(
-          password_manager::features::kWellKnownChangePassword)) {
-    // TODO(crbug.com/927473): Make request to non existing resource and check
-    // if well-known/change-password is supported.
+  GURL request_url = net::GURLWithNSURL(request.URL);
+  // The custom behaviour is only used if the .well-known/change-password
+  // request if the request is the main frame opened in a new tab.
+  if (processing_state_ == kWaitingForFirstRequest) {
+    // Boolean order important, feature flag last. Otherwise it messes
+    // with usage statistics of control and experimental group (UMA).
+    if (request_info.target_frame_is_main &&
+        web_state_->GetLastCommittedURL().is_empty() &&  // empty tab history
+        IsWellKnownChangePasswordUrl(request_url) &&
+        base::FeatureList::IsEnabled(
+            password_manager::features::kWellKnownChangePassword)) {
+      request_url_ = request_url;
+
+      auto url_loader_factory =
+          web_state_->GetBrowserState()->GetSharedURLLoaderFactory();
+      well_known_change_password_state_.FetchNonExistingResource(
+          url_loader_factory.get(), request_url);
+      processing_state_ = kWaitingForResponse;
+    } else {
+      processing_state_ = kInactive;
+    }
   }
+
   return web::WebStatePolicyDecider::PolicyDecision::Allow();
 }
 
@@ -56,21 +75,31 @@ void WellKnownChangePasswordTabHelper::ShouldAllowResponse(
     NSURLResponse* response,
     bool for_main_frame,
     web::WebStatePolicyDecider::PolicyDecisionCallback callback) {
-  const GURL& url = net::GURLWithNSURL(response.URL);
-  // Boolean order important, feature flag check last. Otherwise it messes
-  // with usage statistics.
-  // We only want to handle main_frame requests to keep consistency with the
-  // NavigationThrottle implementation.
-  if (!for_main_frame || !IsWellKnownChangePasswordUrl(url) ||
-      !base::FeatureList::IsEnabled(
+  GURL url = net::GURLWithNSURL(response.URL);
+  // True if the TabHelper expects the response from .well-known/change-password
+  // and only that navigation was started.
+  if (processing_state_ == kWaitingForResponse &&
+      [response isKindOfClass:[NSHTTPURLResponse class]] &&
+      base::FeatureList::IsEnabled(
           password_manager::features::kWellKnownChangePassword)) {
-    // TODO(crbug.com/927473): Handle .well-known/change-passord response.
+    processing_state_ = kResponesReceived;
+    DCHECK(url.SchemeIsHTTPOrHTTPS());
+    response_policy_callback_ = std::move(callback);
+
+    well_known_change_password_state_.SetChangePasswordResponseCode(
+        static_cast<NSHTTPURLResponse*>(response).statusCode);
+  } else {
     std::move(callback).Run(
         web::WebStatePolicyDecider::PolicyDecision::Allow());
-    return;
   }
-  // TODO(crbug.com/927473): Handle Response
-  std::move(callback).Run(web::WebStatePolicyDecider::PolicyDecision::Allow());
+}
+
+void WellKnownChangePasswordTabHelper::RenderProcessGone(
+    web::WebState* web_state) {
+  if (!response_policy_callback_)
+    return;
+  std::move(response_policy_callback_)
+      .Run(web::WebStatePolicyDecider::PolicyDecision::Cancel());
 }
 
 void WellKnownChangePasswordTabHelper::WebStateDestroyed() {}
@@ -78,6 +107,33 @@ void WellKnownChangePasswordTabHelper::WebStateDestroyed() {}
 void WellKnownChangePasswordTabHelper::WebStateDestroyed(
     web::WebState* web_state) {
   web_state->RemoveObserver(this);
+  if (!response_policy_callback_)
+    return;
+  std::move(response_policy_callback_)
+      .Run(web::WebStatePolicyDecider::PolicyDecision::Cancel());
+}
+
+void WellKnownChangePasswordTabHelper::OnProcessingFinished(bool is_supported) {
+  if (!response_policy_callback_)
+    return;
+  if (is_supported) {
+    std::move(response_policy_callback_)
+        .Run(web::WebStatePolicyDecider::PolicyDecision::Allow());
+  } else {
+    std::move(response_policy_callback_)
+        .Run(web::WebStatePolicyDecider::PolicyDecision::Cancel());
+    Redirect(request_url_.GetOrigin());
+  }
+}
+
+void WellKnownChangePasswordTabHelper::Redirect(const GURL& url) {
+  // Making sure there was no other navigation started we could override.
+  if (processing_state_ == kResponesReceived) {
+    web::WebState::OpenURLParams params(url, web::Referrer(),
+                                        WindowOpenDisposition::CURRENT_TAB,
+                                        ui::PAGE_TRANSITION_LINK, false);
+    web_state_->OpenURL(params);
+  }
 }
 
 WEB_STATE_USER_DATA_KEY_IMPL(WellKnownChangePasswordTabHelper)
